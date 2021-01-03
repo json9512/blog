ca8e957c7aeb31ac2702f00952e1c704684b4b01
json9512 <json9512@users.noreply.github.com>
2021-01-03 12:03:51 +0000

    deploy: e32d3ee87cb7632c254efacacfa7bed917e770ae

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated. Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-title.MARKUP Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look at the source for this post to get an idea about how it works. Jekyll also offers powerful support for code snippets: def print_hi(name)  puts \"Hi, #{name}\"endprint_hi('Tom')#=&gt; prints 'Hi, Tom' to STDOUT.Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk. ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "https://json9512.github.io/blog/jekyll/update/welcome-to-jekyll/"
+        "title": "Hello there 👋",
+        "excerpt":"I’m not really sure if I’ll maintain this dev blog in Eng or Kor, but this is my first post. Just to keep track of dev stuff I do. ","categories": [],
+        "tags": ["devblog"],
+        "url": "https://json9512.github.io/blog/first-post/"
       }]
